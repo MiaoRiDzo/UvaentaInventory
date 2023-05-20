71a8ecb2df83c8d088954afa0921443b05c102e0
MiaoRiDzo <ya.eltyshev@gmail.com>
2023-05-20 19:31:19 +0500

    [UPDATE 0.1.3a] *Исправление багов +Добавлено отображение пользователй +Добавлено Добавление, Редактирование, Удаление пользователей ------------------------------- Основная разработка ф-ций закончена. Осталось структурировать проект, пофиксить пару багов, протестировать, а так же добавить необязательную функццию добавления запросов и репортов --------------------------------

---
+++ UvaentaInventory/Resources/Pages/tablesPages/editPages/userEdit.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using UvaentaInventory.Resources.Classes;
+using UvaentaInventory.Resources.Windows;
+
+namespace UvaentaInventory.Resources.Pages.tablesPages.editPages
+{
+    /// <summary>
+    /// Логика взаимодействия для userEdit.xaml
+    /// </summary>
+    public partial class userEdit : Page
+    {
+        MainWindow win;
+        private User _current = new User();
+        bool newIns = true;
+        public userEdit(User current , MainWindow _win)
+        {
+            InitializeComponent();
+            win = _win;
+            if(current != null ) { 
+                _current = current;
+                newIns = false;
+            }
+            DataContext = current;
+            cbRole.ItemsSource = EquipmentUventaEntities.getContext().Role.ToList();
+        }
+
+        private static int getNextId(User current)
+        {
+            try
+            {
+                int id = EquipmentUventaEntities.getContext().User.ToList().Last().UserID;
+                return id + 1;
+            }
+            catch { return 0; }
+        }
+
+        private void saveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder err = new StringBuilder();
+            if (string.IsNullOrEmpty(tbUserName.Text)) { err.AppendLine("Имя пользователся пустое"); }
+            if (string.IsNullOrEmpty(tbLogin.Text)) { err.AppendLine("Логин пустой"); }
+            if(string.IsNullOrEmpty(tbPass.Text)) { err.AppendLine("Пароль пустой"); }
+            if(cbRole.SelectedItem == null) { err.AppendLine("Не выбрана роль пользователя"); }
+            if(err.Length > 0) { MessageBox.Show(err.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error); }
+
+            if (_current.UserID >= 0)
+            {
+                if (newIns)
+                {
+                    _current.UserID = getNextId(_current);
+                    Role role = cbRole.SelectedItem as Role;
+                    _current.RoleID = role.RoleID;
+                }
+                EquipmentUventaEntities.getContext().User.AddOrUpdate(_current);
+                try
+                {
+                    EquipmentUventaEntities.getContext().SaveChanges();
+                    MessageBox.Show("Данные сохранены.");
+                    win.mFrame.GoBack();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка\n" + ex.Message);
+                }
+            }
+        }
+    }
+
+}
--- UvaentaInventory/Resources/Pages/tablesPages/usersPage.xaml.cs
+++ UvaentaInventory/Resources/Pages/tablesPages/usersPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UvaentaInventory.Resources.Classes;
 using UvaentaInventory.Resources.Windows;
 
 namespace UvaentaInventory.Resources.Pages.tablesPages
@@ -26,6 +27,7 @@ namespace UvaentaInventory.Resources.Pages.tablesPages
         {
             InitializeComponent();
             win = _win;
+            userGrid.ItemsSource = EquipmentUventaEntities.getContext().User.ToList();
         }
 
         private void roleBtn_Click(object sender, RoutedEventArgs e)
@@ -35,27 +37,53 @@ namespace UvaentaInventory.Resources.Pages.tablesPages
 
         private void addBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            win.mFrame.Navigate(new Pages.tablesPages.editPages.userEdit(null, win));
         }
 
         private void delBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            var removes = userGrid.SelectedItems.Cast<User>().ToList();
+            if (MessageBox.Show($"Вы точно хотите удалить следующие {removes.Count()} элементы?", "Удаление", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    EquipmentUventaEntities.getContext().User.RemoveRange(removes);
+                    EquipmentUventaEntities.getContext().SaveChanges();
+                    MessageBox.Show("Данные удалены");
+                    userGrid.ItemsSource = EquipmentUventaEntities.getContext().User.ToList();
+                }
+                catch (Exception ex) { MessageBox.Show(ex.Message.ToString()); }
+            }
         }
 
         private void editBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            win.mFrame.Navigate(new Pages.tablesPages.editPages.userEdit((sender as Button).DataContext as User, win));
         }
 
         private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
-
+            EquipmentUventaEntities.getContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+            userGrid.ItemsSource = EquipmentUventaEntities.getContext().User.ToList();
         }
 
         private void tbSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            if (!string.IsNullOrEmpty(tbSearch.Text))
+            {
+                // Предположим, что у вас есть DataGridView с именем dgUsers
+                List<User> list = userGrid.ItemsSource as List<User>; // Получаем List<User> из DataSource
+                if (list != null) // Проверяем, что он не null
+                {
+                    string filter = tbSearch.Text; // Получаем текст из TextBox
+                    List<User> filteredList = list.FindAll(user => user.UserName.Contains(filter)); // Создаем отфильтрованный список по лямбда-выражению
+                    userGrid.ItemsSource = filteredList; // Привязываем отфильтрованный список к DataGridView
+                }
+            }
+            else
+            {
+                userGrid.ItemsSource = EquipmentUventaEntities.getContext().User.ToList();
+            }
         }
     }
 }
